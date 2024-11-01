8ce0738904d908fdaab1e8386300f6244a0fc3bc
anaclrra <anaclarab826@gmail.com>
2024-10-31 22:52:03 -0300

    feat: adiciona model Livro

--- src/main/java/model/Livro.java
+++ src/main/java/model/Livro.java
@@ -1,4 +1,76 @@
 package model;
 
 public class Livro {
+    private int id;
+    private String titulo;
+    private String autor;
+    private int edicao;
+    private String editora;
+    private int ano;
+    private String codigo;
+    private int exemplares;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getEdicao() {
+        return edicao;
+    }
+
+    public void setEdicao(int edicao) {
+        this.edicao = edicao;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getExemplares() {
+        return exemplares;
+    }
+
+    public void setExemplares(int exemplares) {
+        this.exemplares = exemplares;
+    }
 }
